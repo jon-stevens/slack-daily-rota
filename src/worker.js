a09b28edb896ca0c64e58002682b0909b5a09194
Jon <jon-stevens@users.noreply.github.com>
2019-10-14 09:28:05 +0100

    some different daily comments
    Because they are starting to repeat!

--- src/worker.js
+++ src/worker.js
@@ -74,7 +74,7 @@ function updateRotaData(dataObj) {
 	});
 }
 
-function generateRandomMessage(id) {
+function generateRandomMessage(id, pro) {
 
 	function getRandomInt(min, max) {
 		min = Math.ceil(min);
@@ -83,31 +83,40 @@ function generateRandomMessage(id) {
 	}
 
 	const messages = [
-		`I hope you're wearing a nappy, because ${id} is doing a standup today! :happy-sad:`,
+        `${id} was originally planning on going to fight some horses at the Extinction Rebellion protests, but ${pro} remembered ${pro} is absolutely terrified of horses and it is ${pos} turn at standup today.`,
+        `If ${id} gets back from the radiator museum in time ${pro} will be doing today's standup`,
+        `Get out of bed and face the day, ${id} will do a standup soon and has demanded that everyone is completely awake and upright and also please no moths allowed, only team memebers thanks`,
+        `Make sure today's socks are are at least fairly robust, and have full elastical integrity (FULL), because ${id} will do the stand up this morning`,
+        `Deep beneath the Earth's core, occasionally sinister Godwit farmer and retired mercenary ${id} first learnt to play the flute. It took over fourteen years, and today we will find out if it was worth it, or just a massive massive waste of time.`,
+        `If only there were a way to report simulation bugs to our ineffable alien overlords. I really want to know why I couldn't find both socks this morning. Oh ${id} does a full standup today.`,
+        `One must resolve oneself to one's cosmic insignificance in order to be a true standupperer. Fortunately ${id} was born fully aware of ${pos} insignificance, having been raised in a sensory deprivation tank for the first 79 years of ${pos} life.`,
+        `Convicted felon and volunrary garden gnome collector ${id} listened to a bagpipe for over three minutes last week and is therefore entirely qualified to give a standup at us this morning without causing undue suffering.`,
+        `I went to see ${id} give a talk about how to practice the surreptitious ownership of a voluntary carpark last night and I have absolutely no idea what is was about or if ${pro} has just gone mad, but we will find out in this morning's standup.` ,
+        `Today you are presented with an opportunity to study the apparent nature of your existence, and if other people actually exist outside of your fevered little mind. ${id} told me this morning that ${pro} has empirical evidence of the physical existence of the universe, so that should be fun`,
+        `When I was a boy, I often dreamt of working in a custard factory. Did you know there are apprenticeships you can do - oh wait ${id} just told me to shut it because ${pro} is doing a standup in a minute and needs to find ${pos} spare socks`,
+        `A cat on both of my arms would be less expensive than th- Oh, hello, ${id} for the standup today thanks`,
+        `I hope you're wearing an empty nappy, because ${id} is doing a standup today! :happy-sad:`,
 		`Put some seatbealts on your ears, because ${id} is totally going to take them on an epic journey in today's standup`,
-		`${id} wants everybody to know that they are going to do a standup today and that everybody better be ready for it :happy-sad:`,
 		`${id} is going to make you question the futility of your own existence in today's standup`,
 		`Clear your diaries because ${id} is going to standup at you today, and it doesn't get much better than that! :excited:`,
 		`Future ${id} just appeared in a dustbin and told me that past ${id} did the best most amazing standup this morning, so make sure you're wearing socks.`,
-		`The seat of your trousers are going to be thinner and more strained than that teabag that I saw Tom punching the other day, becasue ${id} is going to do a standup! :tea:`,
-		`Imagine a moth doing a standup! How rubbish would that be! Luckily ${id} is doing it today, which will be marginally more exciting. :happy-sad:`,
-		`If Chuck Norris did standup, he might go to ${id} for advice because ${id} is super at doing standups. It's not the best life skill but its better than nothing.`,
-		`Last time ${id} did a standup, my socks fell off and I had a dream about plums. So I'm looking forward to this morning, it will temporarily alleviate the strain of a transient existence. :happy-sad:`,
+        `The seat of your trousers are going to be thinner and more strained than that teabag that I saw Tom punching the other day, becasue ${id} is going to do a standup! :tea:`,
+        `Are you feeling uncomfortable today, and shifting about awkwardly, like a rusty gearbox? Me too. My shoes used to be made of cats though. ${id} today for a standup! ${pro} would never wear cat shoes.`
+		`Imagine a moth doing a standup! How rubbish would that be! Luckily ${id} is doing it today, and not a moth. `,
+		`Last time ${id} did a standup, my socks fell off and I had a dream about plums. So I'm looking forward to this morning. :happy-sad:`,
 		`Last time Rakesh pushed me down the stairs, ${id} came to me in a dream and said don't worry. One day I will be doing a standup and you will forget about your broken legs. ${id} we love you. :boxing_glove:`,
 		`Have you forgotten the sensation of joy? Have you woken up today feeling like a wooden ladder? Don't worry, ${id} will run today's standup and we are all going to feel good again.`,
 		`Make standups great again, ${id}! Bless your tiny tiny miraculous face`,
-		`${id} is doing a standup today, and after that big fight they had on the train this morning, it should be a lively one :ballmer:`,
-		`${id} is doing the full standup experience today, remember not to mention how they tried to force Jovi to live in the fridge for a week, we're not supposed to mention that. :jackfruit:`,
-		`All the rumours about ${id} are untrue, they are doing a standup this morning, and isn't even allergic to eggs. :egg:`,
-		`Remember the last time ${id} did the standup, and there was a big fight at the end, and Dmitry took his shoes off and chased everyone Oh wait that was a dream. I think it probably won't be that good. :dmitryk:`,
-		`I heard ${id} was caught trying to iron a homeless dog this morning. I hope they are still OK to do the standup.`,
-		`When ${id} does the standup it feels like you are slipping into an enormous warm quiche, and before long you start wishing you had been born this way, why why am I a shell-less human? Destined for nothing but suffering and confusion? I want to be have a quiche-shell.`,
+		`${id} is doing a standup today, and after that big fight ${pro} had on the train this morning, it should be a lively one :ballmer:`,
+		`${id} is doing the full standup experience today, remember not to mention how ${pro} tried to force Jovi to live in the fridge for a week, we're not supposed to mention that. :jackfruit:`,
+		`All the rumours about ${id} are untrue, ${pro} is doing a standup this morning, and isn't even allergic to eggs, and wants everyone to know ${pro} has never even been to a B&Q anyway. :egg:`,
+		`Remember the last time ${id} did the standup, and there was a big fight at the end, and Dmitry took his shoes off and chased everyone until they died. I think it probably won't be that good today. :dmitryk:`,
+		`I heard ${id} was caught trying to iron a homeless dog this morning. I hope ${pro} is still OK to do the standup.`,
+		`When ${id} does the standup it feels like you are slipping into an enormous warm quiche, and before long you start wishing you had been born this way, why why was I born this way?`,
 		`${id} has a voice like a beautiful warm wind farm, and today we are going to be filled with renewable energy, after today's STANDUP!`,
 		`Take me far away, and put some cheese in my ears, because ${id} is doing the standup today. I am filled with dread.`,
 		`Whenever ${id} does the standup, my feet shift uncomfortably, like an embarrassed gearbox. But it is not from boredom, or guilt, or worrying about that unresolved blood debt. It's because ${id} has a voice like a bassoon.`,
 		`:cat2: is doing a standup this morning. Oh wait it's ${id} I think. Not :cat2: who is a cat and therefore a total dumbo. :happy-sad:`,
-		`${id} is presenting today's standup, live from Nottingham County Court, where they are facing charges of trying to demolish their local newsagents with a stolen steamroller.`,
-		`${id} is going to do today's standup in French. So I hope you speak French. :flag-fr:`,
 		`In addition to running standup today, ${id} will also be inspecting every team member's socks :socks:`
 	];
 
@@ -118,47 +127,69 @@ class WhosNext {
 	constructor(slackMessage) {
 		this.people = [{
 			name: 'Colleen',
-			username: '<@U0GJL3M8D>'
+			username: '<@U0GJL3M8D>',
+            pro: 'she',
+            pos: 'her'
 		},
 		{
 			name: 'Amit',
-			username: '<@UHV8K7VN3>'
+			username: '<@UHV8K7VN3>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Tom',
-			username: '<@U14GHSETH>'
+			username: '<@U14GHSETH>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Slava',
-			username: '<@UEBPZBRR9>'
+			username: '<@UEBPZBRR9>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Rakesh',
-			username: '<@U0GJA3NF6>'
+			username: '<@U0GJA3NF6>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Leandro',
-			username: '<@UCJLAS1QE>'
+			username: '<@UCJLAS1QE>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Jon S.',
-			username: '<@U7JSC2P9S>'
+			username: '<@U7JSC2P9S>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Anibe',
-			username: '<@U83QSM30D>'
+			username: '<@U83QSM30D>',
+            pro: 'he',
+            pos: 'his'
 		},
 		{
 			name: 'Mirren',
-			username: '<@UDQQ3RHPF>'
+			username: '<@UDQQ3RHPF>',
+            pro: 'she',
+            pos: 'her'
 		},
 		{
 			name: 'Isabel',
-			username: '<@UGNC53L3V>'
+			username: '<@UGNC53L3V>',
+            pro: 'she',
+            pos: 'her'
 		},
 		{
 			name: 'Firas',
-			username: '<@UNS70DR0D>'
+			username: '<@UNS70DR0D>',
+            pro: 'he',
+            pos: 'his'
 		}];
 		this.nonWeekDays = [6, 0]; // Saturday (6) and Sunday (0)
 		this.nonWorkDates = ['Thu Aug 22 2019', 'Mon Aug 26 2019']; // Public holidays and one-off out of office dates
@@ -174,7 +205,7 @@ class WhosNext {
 		const isTodayOfficeHoliday = this.nonWorkDates.includes(today.toDateString());
 		if (!isTodayWeekend && !isTodayOfficeHoliday) {
 			const person = await this._getActivePerson();
-			const msgTxt = generateRandomMessage(`*${person.name}* (${person.username})`);
+			const msgTxt = generateRandomMessage(`*${person.name}* (${person.username})`, person.pro);
 			const blocks = [
 				{
 					type: 'section',
@@ -196,7 +227,7 @@ class WhosNext {
 							}
 						}
 					]
-				}								
+				}
 			];
 
 			if (this.requester) {
